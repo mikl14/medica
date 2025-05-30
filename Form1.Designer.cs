4352f44b1c2d0e77227f131b60a98c0fd6f6801a
mikl14 <mikl.eli2002@gmail.com>
2025-05-30 17:34:31 +0300

    Минорные изменения

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ucEchocardioscopy1 = new WindowsFormsApp1.ucEchocardioscopy();
             this.micBox = new System.Windows.Forms.ComboBox();
             this.stopButton = new System.Windows.Forms.Button();
             this.startButton = new System.Windows.Forms.Button();
+            this.ucEchocardioscopy1 = new WindowsFormsApp1.ucEchocardioscopy();
             this.SuspendLayout();
+            // 
+            // micBox
+            // 
+            this.micBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.micBox.FormattingEnabled = true;
+            this.micBox.Location = new System.Drawing.Point(1104, 403);
+            this.micBox.Name = "micBox";
+            this.micBox.Size = new System.Drawing.Size(121, 24);
+            this.micBox.TabIndex = 8;
+            this.micBox.SelectedIndexChanged += new System.EventHandler(this.micBox_SelectedIndexChanged_1);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.stopButton.Location = new System.Drawing.Point(1194, 471);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(179, 23);
+            this.stopButton.TabIndex = 7;
+            this.stopButton.Text = "Конец записи";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.startButton.Location = new System.Drawing.Point(980, 471);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(194, 23);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Старт записи";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // ucEchocardioscopy1
             // 
@@ -46,38 +78,6 @@
             this.ucEchocardioscopy1.Size = new System.Drawing.Size(1434, 1108);
             this.ucEchocardioscopy1.TabIndex = 0;
             this.ucEchocardioscopy1.Load += new System.EventHandler(this.ucEchocardioscopy1_Load);
-            // 
-            // micBox
-            // 
-            this.micBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.micBox.FormattingEnabled = true;
-            this.micBox.Location = new System.Drawing.Point(1134, 403);
-            this.micBox.Name = "micBox";
-            this.micBox.Size = new System.Drawing.Size(121, 24);
-            this.micBox.TabIndex = 8;
-            this.micBox.SelectedIndexChanged += new System.EventHandler(this.micBox_SelectedIndexChanged_1);
-            // 
-            // stopButton
-            // 
-            this.stopButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.stopButton.Location = new System.Drawing.Point(1224, 471);
-            this.stopButton.Name = "stopButton";
-            this.stopButton.Size = new System.Drawing.Size(179, 23);
-            this.stopButton.TabIndex = 7;
-            this.stopButton.Text = "Конец записи";
-            this.stopButton.UseVisualStyleBackColor = true;
-            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
-            // 
-            // startButton
-            // 
-            this.startButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.startButton.Location = new System.Drawing.Point(1010, 471);
-            this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(194, 23);
-            this.startButton.TabIndex = 6;
-            this.startButton.Text = "Старт записи";
-            this.startButton.UseVisualStyleBackColor = true;
-            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // Form1
             // 
